e18088b2a15094709fc2aa867a75cfd82d6f0c32
Garen Sahagian <g.s.sahagian@gmail.com>
2020-02-18 19:06:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def my_hash
 end
 
 def pioneer
-  pioneer={:name=>"Grace Hopper"}
+  pioneer={:name =>"Grace Hopper"}
 end
 
 def id_generator
@@ -15,7 +15,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  new_hash={key=>value}
 end
 
 def read_from_hash(hash, key)
