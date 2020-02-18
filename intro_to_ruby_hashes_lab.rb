614f962502b911ab83d1090518043b7754238b74
Garen Sahagian <g.s.sahagian@gmail.com>
2020-02-18 19:06:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,15 +11,15 @@ def pioneer
 end
 
 def id_generator
-  id = {:id=>7}
+  id = {:id => 7}
 end
 
 def my_hash_creator(key, value)
-  new_hash={key=>value}
+  new_hash={key =>value}
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
