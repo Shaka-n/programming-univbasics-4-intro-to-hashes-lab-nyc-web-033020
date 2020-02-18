5f9bf69c284fd2e81711deec13163c3c9243fd4b
Garen Sahagian <g.s.sahagian@gmail.com>
2020-02-18 19:05:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,15 +3,15 @@ def new_hash
 end
 
 def my_hash
-  my_hash={}
+  my_hash={my:"hash"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer={:name=>"Grace Hopper"}
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id = {:id=>7}
 end
 
 def my_hash_creator(key, value)
